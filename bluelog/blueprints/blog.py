fb85bff3d27b7c11c8b1c2ab004f2c3208b08a23
jackyang <jackyang@linux.com>
2019-08-09 07:34:29 +0000

    add utils.py

--- bluelog/blueprints/blog.py
+++ bluelog/blueprints/blog.py
@@ -1,3 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blog_bp = Blueprint('blog', __name__)
+
+
+@blog_bp.route('/')
+def index():
+    return render_template('blog/index.html')
+
+@blog_bp.route('/about')
+def about():
+    return render_template('blog/about.html')
+
+@blog_bp.route('/category/<int:category_id>')
+def show_categroy(category_id):
+    return render_template('blog/category.html')
+
+@blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    return render_template('blog/post.html')
