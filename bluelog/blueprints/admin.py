ee051ebe6bd87bf99603cf8787c5170760dd4059
jackyang <jackyang@linux.com>
2019-08-13 08:16:45 +0000

    manage post

--- bluelog/blueprints/admin.py
+++ bluelog/blueprints/admin.py
@@ -12,3 +12,50 @@ def manage_post():
             page, per_page=current_app.config['BLUELOG_MANAGE_POST_PER_PAGE'])
     posts = pagination.items
     return render_template('admin/manage_post.html', pagination=pagination, posts=posts)
+
+
+@admin_bp.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        category = Category.query.get(form.category.data)
+        post = Post(title=title, body=body, category=category)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created.', 'success')
+        return redirect(url_for('blog.show_post', post_id=post.id))
+    return render_template('admin/new_post.html', form=form)
+
+
+@admin_bp.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    form = PostForm()
+    post = Post.query.get_or_404(post_id)
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        category = Category.query.get(form.category.data)
+        post = Post(title=title, body=body, category=category)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post edited.', 'success')
+        return redirect(url_for('blog.show_post', post_id=post.id))
+    form.title.data = post.title
+    form.body.data = post.body
+    form.category.data = post.category_id
+    return render_template('admin/new_post.html', form=form)
+
+
+
+@admin_bp.route('/post/delete/<int:post_id>', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post deleted', 'success')
+    return redirect_back()
